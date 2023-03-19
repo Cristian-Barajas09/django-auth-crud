6df9f596db407e67eed5663bf7588ec5d2941f8f
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2023-03-18 22:19:07 -0400

    update settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': config(
+    'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600)
 }
